21b44f97fc86402ddf5fc380e6704f15b8ae38a2
Sreenu Gopala <sreenugopala007@outlook.com>
2018-02-09 11:39:16 +0530

    Baseline commit

---
+++ src/main/java/com/sreenivaasamu/demoz/j9/modulesdemo/App.java
@@ -0,0 +1,13 @@
+package com.sreenivaasamu.demoz.j9.modulesdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
